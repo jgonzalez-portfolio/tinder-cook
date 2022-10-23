f6c45c97fb44465424b8c8d36c7ccd54b06db8d3
Jonatan Gonzalez <36254816+joni-iOSDev@users.noreply.github.com>
2022-10-22 21:24:59 -0300

    Initial Commit

---
+++ TinderCook/TinderCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCookApp.swift
+//  TinderCook
+//
+//  Created by Joni Gonzalez on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TinderCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
